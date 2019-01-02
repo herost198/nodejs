2475e56a41b5935e6443eef082c4abbecac80acf
Quang Anh <herost198@gmail.com>
2019-01-02 22:58:26 +0700

    test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('')
+
 app.get('/',(req, res) => res.send("hello world"));
+
+
 
 app.listen(3000, ()=> console.log("app listening  on port 3000"))
