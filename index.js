7a6b5788518af1c2782826cbd5002ee1a8f0cbf8
Quang Anh <herost198@gmail.com>
2019-01-02 23:03:47 +0700

    test

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var app = express();
 app.set('view engine', 'pug')
 app.set('views',"./views")
 
-app.get('/',(req, res) => res.send("hello world"));
+app.get('/',function(req, res) {
+    res.send("hello world");
+});
 
+app.get('/user', function(req, res){
+    res.send("User list")
+})
 
 
 app.listen(3000, ()=> console.log("app listening  on port 3000"))
