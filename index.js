79dffe59dd68e2002655f12e60901e00d9eafe2f
Quang Anh <herost198@gmail.com>
2019-01-06 13:55:31 +0700

    bai7

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var low = require('lowdb');
+var FileSync =  require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+
+ db = low(adapter);
+db.defaults({ users: [] })
+   .write();
 
 var app = express();
 
@@ -9,11 +16,11 @@ app.set('views',"./views")
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-var users = [
-    {id: 1, name: "Thinh"},
-    {id: 2, name: "Anh"},
-    {id: 3, name: "Quang"}
-]
+// var users = [
+//     {id: 1, name: "Thinh"},
+//     {id: 2, name: "Anh"},
+//     {id: 3, name: "Quang"}
+// ]
 
 app.get('/',function(req, res) {
     res.render('index',{
@@ -23,7 +30,7 @@ app.get('/',function(req, res) {
 
 app.get('/users', function(req, res){
     res.render('users/index',{
-        users: users
+        users: db.get('users').value()
     });
 })
 
@@ -42,8 +49,19 @@ app.get('/users/create', function(req, res){
     res.render('users/create');
 })
 
+app.get('/users/:id', function(req,res){
+    var id =req.params.id;
+
+    var user = db.get('users').find({id:id}).value();
+    console.log(user);
+    res.render('users/view',{
+        user: user
+    })
+})
+
 app.post('/users/create', function(req, res){
-    users.push(req.body);
+    // users.push(req.body);
+    db.get('users').push(req.body).write();
     console.log(req.body);
     res.redirect('/users');
 })
